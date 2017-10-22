5c9172020abad00e23a8d1c96a75642cfab16540
Jack DuPlessis <jduplessis294@gmail.com>
2017-10-21 22:25:46 -0400

    Add stockdata functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
-const stockData = require('./stockdata.js')
+const stockData = require('../js/stockdata')
 
-stockData.getStockData('GOOG')
-  .then(function(stock) {
-    console.log(stock)
+const symbols = ['VIOO', 'GOOG']
+
+let displayStockData = function (stockSymbols) {
+  document.body.innerHTML = ''
+  stockSymbols.forEach(function (symbol) {
+    stockData.getCurrentStockData(symbol)
+      .then(function(stock) {
+        console.log(stock)
+        document.write(symbol)
+        document.write('<br>')
+        document.write(stock.percent)
+        document.write('<br>')
+        document.write(stock.price)
+        document.write('<br>')
+      })
+      .catch(function(error) {
+        console.error(error)
+      })
   })
-  .catch(function(error) {
-    console.error(error)
+}
+
+stockData.getHistoricalStockData('GOOG', '2016-12-05')
+  .then(function (stock) {
+    console.log(stock.price)
   })
+
+//setInterval(function () {
+  //displayStockData(symbols)
+//}, 5000)
--- js/stockdata.js
+++ js/stockdata.js
@@ -2,28 +2,48 @@ const moment = require('moment')
 const rp = require('request-promise')
 const cheerio = require('cheerio')
 
-let stocks = (function() {
+let stockdata = (function() {
   const KEY="C9AQ"
 
-  let getStockData = function(symbol) {
-    return new Promise (function(resolve, reject) {
+  let getCurrentStockData = function(symbol) {
+    return new Promise (function (resolve, reject) {
       rp(`https://finance.google.com/finance?q=${symbol}`)
         .then(function (htmlString) {
           $ = cheerio.load(htmlString)
-          resolve($('.pr').find('span').text())
+          let result = {}
+          // Gets current price of stock from google website
+          result.price = $('.pr').find('span').text()
+          // Gets percent string and parses it into number after slicing away unneeded characters
+          result.percent = parseFloat(
+            $('.ch.bld').children()[1].children[0].data
+            .slice(1,-1)
+          )
+          resolve(result)
         })
         .catch(function (err) {
         })
     })
   }
 
-  let getCurrentPrice = function(symbol, callback) {
+  let getHistoricalStockData = function(symbol, date) {
+    return new Promise (function (resolve, reject) {
+      rp('https://www.alphavantage.co/query?'+
+         `function=TIME_SERIES_DAILY&outputsize=full&symbol=${symbol}&apikey=${KEY}`
+      ).then(function (jsonString) {
+        json = JSON.parse(jsonString)['Time Series (Daily)'][date]
+        let result = {}
+        result.price = json['4. close']
+        resolve(result)
+      })
+      .catch(function (err) {
+      })
+    })
   }
 
   return {
-    getStockData,
-    getCurrentPrice
+    getCurrentStockData,
+    getHistoricalStockData
   }
 })()
 
-module.exports = stocks
+module.exports = stockdata
